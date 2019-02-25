eae4c23d3472edf75ec68b9ee32217bdbfb5dd98
Luiz Fernando Dias <luizfdias@macbook.local>
2019-02-25 17:36:31 -0300

    Base project

---
+++ Read Faster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Read Faster
+//
+//  Created by f6476359 on 25/02/19.
+//  Copyright © 2019 Luiz Fernando Dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Read FasterTests/Read_FasterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Read_FasterTests.swift
+//  Read FasterTests
+//
+//  Created by f6476359 on 25/02/19.
+//  Copyright © 2019 Luiz Fernando Dias. All rights reserved.
+//
+
+import XCTest
+@testable import Read_Faster
+
+class Read_FasterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
